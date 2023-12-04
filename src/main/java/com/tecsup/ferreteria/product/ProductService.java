f13011bf59fb870560785ccff761c41d3807328d
ShinjiMC <nayapbrau@gmail.com>
2023-12-03 23:27:51 -0500

    Adding EDIT, DELETE AND SELECT of Product

--- src/main/java/com/tecsup/ferreteria/product/ProductService.java
+++ src/main/java/com/tecsup/ferreteria/product/ProductService.java
@@ -1,7 +1,15 @@
 package com.tecsup.ferreteria.product;
 
+import java.util.List;
+
 public interface ProductService {
     Product createProduct(Product product);
 
     Product getProductById(Long productId);
+
+    Product updateProduct(Product product);
+
+    void deleteProduct(Long productId);
+
+    List<Product> getAllProducts(String keyWord);
 }
--- src/main/java/com/tecsup/ferreteria/product/ProductServiceImpl.java
+++ src/main/java/com/tecsup/ferreteria/product/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tecsup.ferreteria.product;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
@@ -16,5 +17,23 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Product getProductById(Long productId) {
         return productRepository.findById(productId).orElse(null);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Long productId) {
+        productRepository.deleteById(productId);
+    }
+
+    @Override
+    public List<Product> getAllProducts(String keyWord) {
+        if (keyWord != null) {
+            return productRepository.findAll(keyWord);
+        }
+        return productRepository.findAll();
     }
 }
