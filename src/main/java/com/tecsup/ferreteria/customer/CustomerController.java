307d401e94d69a105f4e0458914a38bea265e3a6
ShinjiMC <nayapbrau@gmail.com>
2023-12-04 23:39:56 -0500

    Hibernate Validator

--- src/main/java/com/tecsup/ferreteria/customer/CustomerController.java
+++ src/main/java/com/tecsup/ferreteria/customer/CustomerController.java
@@ -2,9 +2,12 @@ package com.tecsup.ferreteria.customer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("api/v1/customers")
@@ -14,7 +17,10 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping("/register")
-    public String createCustomer(@ModelAttribute("customer") Customer customer) {
+    public String createCustomer(@ModelAttribute("customer") @Valid Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
         customerService.createCustomer(customer);
         return "redirect:/profile";
     }
